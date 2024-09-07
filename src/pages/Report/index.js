ec51becc16b426a21edc429bfcbb174d1bef269a
Abhay Sharma <abhaysharmajr@gmail.com>
2024-09-07 23:02:55 +0530

    created basic design and components for result page

--- src/pages/Report/index.js
+++ src/pages/Report/index.js
@@ -1,7 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import styles from "../Report/report.module.css";
+import ResponseBox from "../../components/ResponseBox";
+import Button from "../../components/Button";
+import CircularProgressBar from "../../components/Circlular Progress Bar";
 export default function Report() {
   return (
-    <div>Report</div>
-  )
+    <div className={styles.reportMainDiv}>
+      <div className={styles.innerLayout}>
+        <h2>Your result</h2>
+        <CircularProgressBar />
+        <ResponseBox />
+        <Button buttonText="Start Again" />
+      </div>
+    </div>
+  );
 }
