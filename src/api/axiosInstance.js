7286b0441369ffe43ec2c7b26f1d7a441656f484
Abhay Sharma <abhaysharmajr@gmail.com>
2024-09-09 00:12:56 +0530

    added url in env and env in gitignore

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://627dfcd0b75a25d3f3af4996.mockapi.io",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
